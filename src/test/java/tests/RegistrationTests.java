53d8bd4eb1e97c124f4e731637ac004903f42985
margaritacernickaa <rita.chernitskaya@gmail.com>
2024-12-02 20:36:29 +0200

    New Project

---
+++ src/test/java/tests/RegistrationTests.java
@@ -0,0 +1,39 @@
+package tests;
+
+import dto.UserDtoLombok;
+import manager.ApplicationManager;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.RegistrationPage;
+import pages.SearchPage;
+
+import java.util.Random;
+
+public class RegistrationTests extends ApplicationManager {
+
+    RegistrationPage registrationPage;
+
+    @Test
+    public void registrationPositiveTest(){
+        int i = new Random().nextInt(1000)+1000;
+        UserDtoLombok user = UserDtoLombok.builder()
+                .name("Bob")
+                .lastName("Doe")
+                .email(i+"bob_mail@gmail.com")
+                .password("Password123!")
+                .build();
+        new SearchPage(getDriver()).clickBtnSignUp();
+        registrationPage = new RegistrationPage(getDriver());
+        registrationPage.typeRegistrationForm(user);
+       // new RegistrationPage(getDriver()).typeRegistrationForm(user);
+        registrationPage.clickCheckBox();
+        registrationPage.clickBtnYalla();
+        Assert.assertTrue(registrationPage.isPopUpMessagePresent());
+
+
+
+
+
+
+    }
+}
